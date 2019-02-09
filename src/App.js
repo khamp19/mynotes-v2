767027b59787f1156bf4ff92aca58479f044be35
Kathryn Hampton <khampton119@gmail.com>
2019-02-09 13:17:25 -0500

    created landing page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import LandingPage from './Components/landingPage';
 
 class App extends Component {
   render() {
@@ -8,18 +10,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>App under construction</p>
         </header>
+        <div>
+          <LandingPage />
+        </div>
       </div>
     );
   }
---
+++ src/Components/landingPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h2>Hello world!</h2>
+      <p>Hey there. This page is currently under construction.</p>
+      <p>A new and improved note-taking app is coming soon!</p>
+      <p>If you would like to see the old version of this project
+        please click here:</p>
+      <p>
+        <a href="https://mynotesproject.herokuapp.com/"
+          target="_blank" rel="noopener noreferrer">
+          My (Original) Notes Project
+        </a>
+      </p>
+    </div>
+  )
+}
+
+export default LandingPage;
