dd2d3cec42ea4a50f0c378b1bfce3a44ae13203e
Kathryn Hampton <khampton119@gmail.com>
2019-02-12 10:24:33 -0500

    more css updates and main auth files

--- src/Components/Authentication/login.js
+++ src/Components/Authentication/login.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Login = () => {
   return (
     <div>
-      <p>hello from login!</p>
+      <p>hello from Login!</p>
+      <p>please log in</p>
     </div>
   )
 }
