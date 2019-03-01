b34be34f422325fecb45253ca777b081a9acc33f
Kathryn Hampton <khampton119@gmail.com>
2019-03-01 11:32:31 -0500

    trying to parse date using momentjs

--- src/Components/Notes/NoteDetail.js
+++ src/Components/Notes/NoteDetail.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
 
 import { getNote } from '../../Actions/NotesActions';
 
 class NoteDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: '',
+    }
+  }
+
   componentDidMount() {
     this.props.getNote(this.props.match.params.id);
-    console.log('note detail props', this.props.note);
+  }
+
+  // MOVE THIS FUNCTION INTO YOUR RENDER METHOD
+  // REMOVE STATE
+  getDate = () => {
+    if(this.props.note.id) {
+    //   const date = this.state;
+    //   let year = moment(date).year();
+    //   let day = moment(date).date();
+    //   let month = moment(date).month();
+    //   let str = moment(`${day}-${month + 1}-${year}`, "Do-MM-YYYY").format('ddd, MMM Do YYYY');
+    //   console.log(str);
+      // this.setState({ date: str });
+      console.log('state date: ', this.state.date);
+    }
   }
 
   render() {
+    const { title, content, created_at } = this.props.note;
+    const { date } = this.state;
     return (
       <div>
         <div>
-          <p>hello from note detail!</p>
-        </div>
-        <div>
-          <p>Note info:</p>
-          <h3>{this.props.note.title}</h3>
-          <p>{this.props.note.content}</p>
-          <p>{this.props.note.created_at}</p>
+          {this.props.getting_note ? <h4>Getting Note</h4> : null}
+          {this.props.error ? <h4>Error getting this note</h4> : null}
+          {
+            <div>
+              <p>Note info:</p>
+              <h3>title: {title}</h3>
+              <p>content: {content}</p>
+              {this.getDate(created_at)}
+              <p>{date}</p>
+            </div>
+          }
         </div>
       </div>
     )
@@ -37,3 +65,4 @@ const mapStateToProps = state => {
 
 
 export default connect(mapStateToProps, { getNote })(NoteDetail);
+
